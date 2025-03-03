c861be82c9df320024e1da2fa1e83582cf975efc
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2025-03-02 21:41:53 -0800

    adding the ebay api to work with the page, first iteration that allows you to look up buy prices

---
+++ src/services/ebayService.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export interface EbaySoldItem {
+    title: string;
+    price: string;
+    currency: string;
+    location: string;
+    endTime: string;
+    viewItemURL: string;
+  }
+
+export async function searchRecentlyBuyProducts(query: string = "charizard", maxResults: number = 10): Promise<EbaySoldItem[]> {
+  try {
+    const response = await axios.get(`http://localhost:5000/api/ebay/buy`, {
+      params: { query, maxResults },
+    });
+
+    const items = response.data.itemSummaries || [];
+    console.log(items);
+    return items;
+  } catch (error) {
+    console.error("Error fetching eBay sold items:", error);
+    return [];
+  }
+}
+
+export async function searchRecentlySoldProducts(query: string = "charizard", maxResults: number = 10): Promise<EbaySoldItem[]> {
+  try {
+    const response = await axios.get(`http://localhost:5000/api/ebay/sold`, {
+      params: { query, maxResults },
+    });
+
+    const items = response.data || [];
+    console.log(items);
+    return items;
+  } catch (error) {
+    console.error("Error fetching eBay sold items:", error);
+    return [];
+  }
+}
