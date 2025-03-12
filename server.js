73cd88ff47063a30f5989a141bc01221365c44dd
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2025-03-11 21:12:24 -0700

    making the env variable different

--- server.js
+++ server.js
@@ -10,23 +10,23 @@ const PORT = 5000;
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-const EBAY_APP_ID = process.env.EBAY_PROD_APP_ID;
-const EBAY_SECRET_ID = process.env.EBAY_PROD_CERT_ID;
-console.log(EBAY_APP_ID);
-console.log(EBAY_SECRET_ID);
+const VITE_EBAY_APP_ID = process.env.VITE_EBAY_PROD_APP_ID;
+const VITE_EBAY_SECRET_ID = process.env.VITE_EBAY_PROD_CERT_ID;
+console.log(VITE_EBAY_APP_ID);
+console.log(VITE_EBAY_SECRET_ID);
 
-const EBAY_OAUTH_URL = "https://api.ebay.com/identity/v1/oauth2/token";
-const EBAY_FINDING_API_URL = "https://api.ebay.com/buy/browse/v1/item_summary/search";
-const EBAY_SOLD_API_URL = "https://svcs.ebay.com/services/search/FindingService/v1";
+const VITE_EBAY_OAUTH_URL = "https://api.ebay.com/identity/v1/oauth2/token";
+const VITE_EBAY_FINDING_API_URL = "https://api.ebay.com/buy/browse/v1/item_summary/search";
+const VITE_EBAY_SOLD_API_URL = "https://svcs.ebay.com/services/search/FindingService/v1";
 
 let token;
 
 const getEbayOAuthToken = async () => {
-  const authString = Buffer.from(`${EBAY_APP_ID}:${EBAY_SECRET_ID}`).toString("base64");
+  const authString = Buffer.from(`${VITE_EBAY_APP_ID}:${VITE_EBAY_SECRET_ID}`).toString("base64");
 
   try {
     const response = await axios.post(
-      EBAY_OAUTH_URL,
+      VITE_EBAY_OAUTH_URL,
       "grant_type=client_credentials&scope=https://api.ebay.com/oauth/api_scope",
       {
         headers: {
@@ -49,17 +49,17 @@ app.get("/api/ebay/buy", async (req, res) => {
     const { query, maxResults = 10 } = req.query;
     console.log("loading")
 
-    const response = await axios.get(EBAY_FINDING_API_URL, {
+    const response = await axios.get(VITE_EBAY_FINDING_API_URL, {
       params: {
         // "category_ids":"183050",
         "q": query,
         "limit": maxResults,
         "sort": "bestMatch",
-        "SECURITY-APPNAME": EBAY_APP_ID,
+        "SECURITY-APPNAME": VITE_EBAY_APP_ID,
       },
       headers: {
         'Authorization': `Bearer ${token}`,
-        'X-EBAY-C-MARKETPLACE-ID':`EBAY_US`,
+        'X-EBAY-C-MARKETPLACE-ID':`VITE_EBAY_US`,
         'X-EBAY-C-ENDUSERCTX':`affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId></referenceId>`,
         "Content-Type": "application/json",
       },
@@ -77,25 +77,25 @@ app.get("/api/ebay/sold", async (req, res) => {
     const { query, maxResults = 10 } = req.query;
     console.log("loading")
 
-    const response = await axios.get(EBAY_SOLD_API_URL, {
+    const response = await axios.get(VITE_EBAY_SOLD_API_URL, {
       params: {
         // "OPERATION-NAME": "findCompletedItems",
         // "SERVICE-VERSION": "1.13.0",
-        // "SECURITY-APPNAME": `${EBAY_APP_ID}`,
+        // "SECURITY-APPNAME": `${VITE_EBAY_APP_ID}`,
         // "keywords": `${query}`,
         // "itemFilter.name": "SoldItemsOnly",
         // "itemFilter.value": "true",
         // "paginationInput.entriesPerPage": `${maxResults}`,
         "OPERATION-NAME": "findItemsByKeywords",
         "SERVICE-VERSION": "1.13.0",
-        "SECURITY-APPNAME": EBAY_APP_ID, // No OAuth, just App ID
+        "SECURITY-APPNAME": VITE_EBAY_APP_ID, // No OAuth, just App ID
         "RESPONSE-DATA-FORMAT": "JSON",
         "REST-PAYLOAD": "",
         "keywords": query,
       },
       headers: {
-        "X-EBAY-SOA-SECURITY-APPNAME": EBAY_APP_ID,
-        'X-EBAY-C-MARKETPLACE-ID':`EBAY_US`,
+        "X-EBAY-SOA-SECURITY-APPNAME": VITE_EBAY_APP_ID,
+        'X-EBAY-C-MARKETPLACE-ID':`VITE_EBAY_US`,
         'X-EBAY-C-ENDUSERCTX':`affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId></referenceId>`,
         "Content-Type": "application/json",
       },
